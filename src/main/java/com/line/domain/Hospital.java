7383a6b5a32098108f7d59a8cef2585bd765724e
MUNJU <mvv0030@gmail.com>
2022-10-13 13:23:07 +0900

    Hospital Parse 1. " 처리 2. test code 추가

--- src/main/java/com/line/domain/Hospital.java
+++ src/main/java/com/line/domain/Hospital.java
@@ -10,13 +10,42 @@ public class Hospital {
     private String subdivision;
 
     // id 받아줄 constructor 생성
-    public Hospital(String id) {
-        this.id = id.replaceAll("\"", "");
+//    public Hospital(String id) {
+//        this.id = id.replaceAll("\"", "");
         // constructor에서 id를 set할 때 "를 빈칸으로
+//    }
+
+    public Hospital(String id, String address) {
+        this.id = id;
+        this.address = address;
     }
 
     // getter 생성
     public String getId() {
         return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getEmergencyRoom() {
+        return emergencyRoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubdivision() {
+        return subdivision;
     }
 }
--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -3,10 +3,16 @@ package com.line.parser;
 import com.line.domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital>{
+
+//    private String replaceAllQuot(String str) {
+//        return  str.replaceAll("\"", "");
+//    } 이 메소드 없애고 밑에 추가해서 라인을 받을 때에 " 없애는 코드 추가
     @Override
-    public Hospital parse(String str) {
+    public Hospital parse(String str) { // 라인을 받자마자 " 삭제
+        str = str.replaceAll("\"", "");
         String[] splitted = str.split(",");
         // csv는 쉼표로 구분되어 있는 데이터이기 때문에 쉼표를 기준으로 데이터 자르기
-        return new Hospital(splitted[0]);
+        return new Hospital(splitted[0], splitted[1]);
+
     }
 }
